240957cdc4eb3ca5dd400e3b548afe195035b689
Lewis Vine <lew.vine@gmail.com>
2023-02-10 11:42:56 -0500

    Created methods for weapons, herd and fleet creation

--- RobotsVDinos/Program.cs
+++ RobotsVDinos/Program.cs
@@ -2,36 +2,10 @@
 
 using RobotsVDinos;
 
-//Create and populate weapons list and weapons
-List<Weapon> weapons = new List<Weapon>();
-
-Weapon sword = new Weapon("sword", 5);
-Weapon halberd = new Weapon("halberd", 10);
-Weapon mace = new Weapon("mace", 15);
-
-weapons.Add(sword);
-weapons.Add(halberd);
-weapons.Add(mace);
-
-//Create and populate herd and dinos
-Herd herd = new Herd();
-
-Dinosaur rex = new Dinosaur("Rex");
-Dinosaur tbone = new Dinosaur("TBone");
-Dinosaur max = new Dinosaur("Max");
-herd.dinosaurs.Add(rex);
-herd.dinosaurs.Add(tbone);
-herd.dinosaurs.Add(max);
-
-//Create and populate fleet and robots
-Fleet fleet = new Fleet();
-
-Robot c3p0 = new Robot("c3po", sword);
-Robot robo = new Robot("robo", sword);
-Robot r2d2 = new Robot("r2d2", sword);
-fleet.robots.Add(c3p0);
-fleet.robots.Add(robo);
-fleet.robots.Add(r2d2);
+//Set up the game
+List<Weapon> weapons = CreateWeapons();
+Herd herd = CreateHerd();
+Fleet fleet = CreateFleet(weapons);
 
 Console.WriteLine("Welcome to Robots versus Dinosaurs!");
 Console.WriteLine("In Robots versus Dinosaurs, you play the Robot, and fight against Dinosaurs!  Each team has three members");
@@ -60,23 +34,71 @@ if (answer == "YES")
     answer = Console.ReadLine();
 }
 
-//WEAPONS LIST
-Console.WriteLine("Here is the list of available weapons:");
-Console.WriteLine();
-Console.WriteLine("NAME      ATTACK POWER");
-Console.WriteLine("----------------------");
-int space;
-foreach (Weapon weapon in weapons)
+List<Weapon> CreateWeapons()
 {
-    string output = weapon.Type.ToUpper() + " ";
-    if(output.Length < 10)
+    //Create and populate weapons list and weapons
+    List<Weapon> weapons = new List<Weapon>();
+
+    Weapon sword = new Weapon("sword", 5);
+    Weapon halberd = new Weapon("halberd", 10);
+    Weapon mace = new Weapon("mace", 15);
+
+    weapons.Add(sword);
+    weapons.Add(halberd);
+    weapons.Add(mace);
+
+    return weapons;
+}
+
+Fleet CreateFleet(List<Weapon> weapons)
+{
+    //Create and populate fleet and robots
+    Fleet fleet = new Fleet();
+
+    Robot c3p0 = new Robot("c3po", weapons[0]);
+    Robot robo = new Robot("robo", weapons[0]);
+    Robot r2d2 = new Robot("r2d2", weapons[0]);
+    fleet.robots.Add(c3p0);
+    fleet.robots.Add(robo);
+    fleet.robots.Add(r2d2);
+
+    return fleet;
+}
+
+Herd CreateHerd()
+{
+    //Create and populate herd and dinos
+    Herd herd = new Herd();
+
+    Dinosaur rex = new Dinosaur("Rex");
+    Dinosaur tbone = new Dinosaur("TBone");
+    Dinosaur max = new Dinosaur("Max");
+    herd.dinosaurs.Add(rex);
+    herd.dinosaurs.Add(tbone);
+    herd.dinosaurs.Add(max);
+
+    return herd;
+}
+
+void ShowWeaponsList()
+{
+    Console.WriteLine("Here is the list of available weapons:");
+    Console.WriteLine();
+    Console.WriteLine("NAME      ATTACK POWER");
+    Console.WriteLine("----------------------");
+    int space;
+    foreach (Weapon weapon in weapons)
     {
-        int count = 10 - output.Length;
-        for(int i = 0; i < count; i++)
+        string output = weapon.Type.ToUpper() + " ";
+        if (output.Length < 10)
         {
-            output += " ";
+            int count = 10 - output.Length;
+            for (int i = 0; i < count; i++)
+            {
+                output += " ";
+            }
         }
+        output += weapon.AttackPower;
+        Console.WriteLine(output);
     }
-    output += weapon.AttackPower;
-    Console.WriteLine(output);
 }
